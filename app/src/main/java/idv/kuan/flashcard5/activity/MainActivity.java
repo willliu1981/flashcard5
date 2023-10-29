f0c407425976cf943b6afcfe2b740e5e217f0d50
willliu1981 <h111107231675>
2023-10-30 02:45:40 +0800

    重構SchemaModifier

--- app/src/main/java/idv/kuan/flashcard5/activity/MainActivity.java
+++ app/src/main/java/idv/kuan/flashcard5/activity/MainActivity.java
@@ -16,6 +16,9 @@ import idv.kuan.kuanandroidlibs.activites.ProxyMainActivity;
 import idv.kuan.kuanandroidlibs.databases.provider.AndroidDBFactory;
 import idv.kuan.libs.databases.DBFactoryCreator;
 import idv.kuan.libs.databases.utils.SQLiteSchemaModifiers;
+import idv.kuan.libs.databases.utils.schema.modifier.SchemaModifier;
+import idv.kuan.libs.databases.utils.schema.modifier.SchemaModifierHandler;
+import idv.kuan.libs.databases.utils.schema.modifier.TableSchemaModifier;
 
 public class MainActivity extends ProxyMainActivity {
 
@@ -50,6 +53,36 @@ public class MainActivity extends ProxyMainActivity {
 
     private void changeTableStructure(int appVersion) {
         Connection connection = DBFactoryCreator.getFactory().getConnection();
+
+        SchemaModifierHandler handler = new SchemaModifierHandler(connection, appVersion);
+        SchemaModifierHandler.SchemaModifierCreator schemaModifierCreator = handler.getSchemaModifierCreator();
+        schemaModifierCreator.setConstructionSql("CREATE TABLE \"word\" ( " +
+                " \"id\" INTEGER NOT NULL UNIQUE, " +
+                " \"term\" TEXT NOT NULL, " +
+                " \"translation\" TEXT NOT NULL, " +
+                " \"translation5\" , " +
+                " \"at_created\" TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
+                " \"at_updated\" TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
+                " PRIMARY KEY(\"id\" AUTOINCREMENT) " +
+                ")");
+
+        TableSchemaModifier schemaModifier = (TableSchemaModifier) schemaModifierCreator.createSchemaModifier();
+        schemaModifier.setTableName("word");
+        schemaModifier.setCurrentColumns("id,term,translation,at_created,at_updated,translation5");
+        schemaModifier.setSelectedColumns("id,term,translation,at_created,at_updated,'N/A5X3'");
+        /* or set columns mapping
+        schemaModifier.setColumnsMapping("id,term,translation,at_created,at_updated,translation5 :" +
+                "id,term,translation,at_created,at_updated,'N/A5X2'");
+        //*/
+
+        handler.addSchemaModifier(schemaModifier);
+
+        handler.execute();
+
+    }
+
+    private void changeTableStructureOld(int appVersion) {
+        Connection connection = DBFactoryCreator.getFactory().getConnection();
         String sql1 = "CREATE TABLE \"word\" ( " +
                 " \"id\" INTEGER NOT NULL UNIQUE, " +
                 " \"term\" TEXT NOT NULL, " +
@@ -60,30 +93,17 @@ public class MainActivity extends ProxyMainActivity {
                 " PRIMARY KEY(\"id\" AUTOINCREMENT) " +
                 ")";
 
-        String partMigrateSql = "id,term,translation,at_created,at_updated,translation5 : " +
-                "id,term,translation,at_created,at_updated,'N/A5X1'";
-
         SQLiteSchemaModifiers.SchemaModifier schemaModifier = SQLiteSchemaModifiers.createSchemaModifier(connection, appVersion);
         schemaModifier.addSchemaModifierExecutor((c, v) -> {
             final String TABLE_NAME = "word";
-            //method1:
-            /*
-            SQLiteSchemaModifiers.createOrUpdateTableWithDataMigration(c,
-                    v, TABLE_NAME, sql1, partMigrateSql);
-            //*/
 
-            //method2:
-            //*
-            SQLiteSchemaModifiers.SchemaModifierSQL schemaModifierSQL = new SQLiteSchemaModifiers.SchemaModifierSQL(TABLE_NAME);
+            SQLiteSchemaModifiers.ColumnsMappingSql schemaModifierSQL = new SQLiteSchemaModifiers.ColumnsMappingSql(TABLE_NAME);
             schemaModifierSQL.createInsertIntoSQL("id,term,translation,at_created,at_updated,translation5",
                     "id,term,translation,at_created,at_updated,'N/A5X2'");
-            SQLiteSchemaModifiers.createOrUpdateTableWithDataMigration(c, v, TABLE_NAME, sql1, schemaModifierSQL);
-            //*/
-            //最後組合出來的sql 可能會是:"INSERT INTO " + tableName + partMigrateSql + " FROM " + tableName + "__temp"
+            //SQLiteSchemaModifiers.createOrUpdateTableWithDataMigration(c, v, TABLE_NAME, sql1, schemaModifierSQL);
         });
 
-
-        schemaModifier.createDatabase();
+        schemaModifier.createOrUpdateDatabase();
 
     }
 
